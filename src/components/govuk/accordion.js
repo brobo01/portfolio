030c20b4a524be8b4652f2158e3dc56d50c4d8a2
Ben Roberts <benroberts67@hotmail.co.uk>
2026-08-17 14:12:54 +0100

    Added gov.uk design system

--- src/components/govuk/accordion.js
+++ src/components/govuk/accordion.js
@@ -1,43 +1,34 @@
 "use client"
 
-import { useEffect, useRef } from "react"
-import { Accordion } from "govuk-frontend"
 import layout from "@/app/theme/layout.module.css"
 
-export default function AccordionComponent({ title, items, id = "accordion" }) {
-  const accordionRef = useRef(null)
-
-  useEffect(() => {
-    if (!accordionRef.current) return
-
-    new Accordion(accordionRef.current)
-  }, [])
-
+export default function AccordionComponent({
+  title,
+  items,
+  id = "accordion-with-summary-sections",
+}) {
   return (
-    <section className={layout.container}>
-      <h1 className="govuk-label govuk-label--l">{title}</h1>
-      <div
-        ref={accordionRef}
-        className="govuk-accordion"
-        data-module="govuk-accordion"
-        id={id}
-      >
-        {items.map((item, index) => (
-          <div className="govuk-accordion__section" key={item.id || index}>
-            <div className="govuk-accordion__section-header">
-              <h2 className="govuk-accordion__section-heading">
-                <span className="govuk-accordion__section-button">
-                  {item.title}
-                </span>
-              </h2>
-            </div>
+    <div>
+      <section className={layout.container}>
+        <h1 className="govuk-label govuk-label--l">{title}</h1>
+        <div className="govuk-accordion" data-module="govuk-accordion" id={id}>
+          {items.map((item, index) => (
+            <div className="govuk-accordion__section" key={item.id || index}>
+              <div className="govuk-accordion__section-header">
+                <h2 className="govuk-accordion__section-heading">
+                  <span className="govuk-accordion__section-button">
+                    {item.title}
+                  </span>
+                </h2>
+              </div>
 
-            <div className="govuk-accordion__section-content">
-              <p className="govuk-body">{item.content}</p>
+              <div className="govuk-accordion__section-content">
+                <p className="govuk-body">{item.content}</p>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
-    </section>
+          ))}
+        </div>
+      </section>
+    </div>
   )
 }
